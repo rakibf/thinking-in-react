03c1adebc6dd9f78238e28978cb4c5d72adc11b3
Rakib Hassan <mailrakibhassan@gmail.com>
2023-01-27 19:53:16 +0600

    react jsx and rendering

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// create a React element // JSX is converted to this createElement calls after babel compiles JSX
+// const element = React.createElement("h1", null, "Hello, world!");
+const index = 0;
+// const element = <h1 className="heading" tabIndex={index}>Hello, world!</h1>
+const element = (
+  <div className="container" tabIndex={index}>
+    {/* ["Hello", "Bangladesh"] */}
+    <h1>Hello, {"Bangla" + "desh"}</h1>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const element2 = React.createElement(
+  "div",
+  { className: "container", tabIndex: index },
+  React.createElement("h1", null, "Hello, ", "Bangla" + "desh")
+);
+
+/* 
+{
+  type: "div",
+  props: {
+    className: "container",
+    tabIndex: "0",
+    children: {
+      type: "h1",
+      props: {
+        children: ["Hello, ", "Bangladesh"]
+      }
+    }
+  }
+
+}
+
+*/
+
+const UnorderedList = (
+  <ul className="list-container">
+    <li>List Item</li>
+  </ul>
+);
+
+console.log(UnorderedList);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<div>{UnorderedList}</div>);
